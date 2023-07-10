7579212e68b579b243e09b7163369c739e20e76e
Rayhan Yuda Lesmana <rayhanyuda13@gmail.com>
2023-07-10 16:32:40 +0700

    Fix about us

--- resources/views/aboutus/index.blade.php
+++ resources/views/aboutus/index.blade.php
@@ -1,102 +1,96 @@
 @extends('app')
 @section('content')
 
+<section>
     <div class="bg-question">
-        <div class="section-aboutus-hero pt-5">
-            <div class="container">
-                <div class="row px-5">
-                    <div class="padding-title col-12 col-lg-4" data-aos="fade-right" data-aos-duration="1000">
-                        <div class="font-hero-title1">
-                            This is
+        <div class="container">
+            <div class="row">
+                <div class="col-12 my-5 px-5">
+                    <div class="row">
+                        <div class="col-12 col-lg-6 p-2 my-5" data-aos="fade-right" data-aos-duration="1000">
+                            <p class="fw-bold" style="font-size:40px;">Introducing <b class="text-warning">X</b><b
+                                    style="color:#1200b1;">10S</b>, Your Gateway to Seamless API Services</p>
                         </div>
-                        <div class="font-hero-title2">
-                            SWAMEDIA
+                        <div class="col-12 col-lg-6 px-3 py-2 rounded my-5" data-aos="fade-left" data-aos-duration="1000"
+                            style="text-align: center; box-shadow: rgba(255, 193, 7, 0.4) 5px 5px, rgba(255, 193, 7, 0.3) 10px 10px, rgba(255, 193, 7, 0.2) 15px 15px, rgba(255, 193, 7, 0.1) 20px 20px, rgba(255, 193, 7, 0.05) 25px 25px;">
+                            <p>At <b class="text-warning">X</b><b style="color:#1200b1;">10S</b>, we understand the importance of providing exceptional API services to our
+                                valued customers. Our mission is to empower businesses like yours by offering a reliable
+                                and user-friendly platform for accessing and utilizing APIs effortlessly.
+                                <br>
+                                With <b class="text-warning">X</b><b style="color:#1200b1;">10S</b>, you gain access to a vast range of API solutions designed to cater to your
+                                specific needs. Whether you're looking to integrate payment gateways, social media
+                                platforms, or data analytics tools, our comprehensive API library has got you covered.
+                                <br>
+                                Say goodbye to the hassle of building and maintaining your own APIs. We've done the
+                                groundwork, so you can focus on what matters most: delivering exceptional products and
+                                services to your customers.</p>
                         </div>
-                    </div>
-                    <div class="col-12 col-lg-8" data-aos="fade-left" data-aos-duration="1000">
-                        <p class="font-hero-content text-end">
-                            SWAMEDIA API adalah solusi satu atap untuk semua kebutuhan keuangan Anda. Dengan API kami, Anda dapat mengakses berbagai layanan keuangan, termasuk pembayaran, transfer, dan lainnya. API kami dirancang agar mudah digunakan, aman, dan andal, sehingga Anda dapat berfokus pada hal terbaik yang Anda lakukan.
-                        </p>
                     </div>
                 </div>
-            </div>
-        </div>
-        {{-- <div class="section-bg-asabri pb-5">
-            <div class="container" data-aos="fade-up" data-aos-duration="1000">
-                <div class="row d-flex justify-content-center px-5">
-                    <div class="col-11">
-                        <div class="card mopishm-card">
-                            <div class="card-body text-center">
-                                <img class="w-100" src="{{ asset ('assets/images/aboutus/asabri.jpg') }}" alt="">
+                <div class="col-12 my-5">
+                    <div class="row">
+                        <div class="col-12 col-lg-12" data-aos="fade-down" data-aos-duration="1000">
+                            <div class="text-center">
+                                <img class="w-50" src="{{ asset ('assets/images/aboutus/asabriapi.png') }}" alt="Picture Asabri API">
+                            </div>
+                        </div>
+                        <div class="col-12" data-aos="fade-up" data-aos-duration="1000">
+                            <div class="text-center">
+                                <p>
+                                    With <b class="text-warning">X</b><b style="color:rgb(18, 0, 177);">10S</b>, you gain access to a vast range of API solutions designed to cater to your specific needs. Whether you're looking to integrate payment gateways, social media platforms, or data analytics tools, our comprehensive API library has got you covered. Say goodbye to the hassle of building and maintaining your own APIs. We've done the groundwork, so you can focus on what matters most: delivering exceptional products and services to your customers.
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div> --}}
-        <div class="section-aboutus-asabriapi">
-            <div class="container">
-                <div class="row pb-5">
-                    <div class="col-12 col-lg-6 text-center" data-aos="fade-up-right" data-aos-duration="1000"> 
-                        <img class="w-100" src="{{ asset ('assets/images/aboutus/asabriapi.png') }}" alt="Picture Asabri API">
-                    </div>
-                    <div class="col-12 col-lg-6" data-aos="fade-down-left" data-aos-duration="1000"> 
-                        <p class="font-asabriapi-content1">SWAMEDIA API</p>
-                        <p class="font-asabriapi-content2">
-                            SWAMEDIA API is your one-stop solution for all your financial needs. With our API, you can access a wide range of financial services, including payments, transfers, and more. Our API is designed to be easy to use, secure, and reliable, so you can focus on what you do best.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="section-aboutus-widget" data-aos="zoom-in-up" data-aos-duration="1000">
-            <div class="container">
-                <div class="row px-5">
-                    <div class="col-12 col-lg-4">
-                        <div class="card bg-transparent shadow">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-12 col-lg-9">
-                                        <p class="font-widget-content1">Transaction</p>
-                                        <p class="font-widget-content2">2500 +</p>
-                                    </div>
-                                    <div class="col-12 col-lg-3">
-                                        <div class="text-center">
-                                            <img class="w-75" src="{{ asset ('assets/images/aboutus/icon-transaction.png') }}" alt="Icon Widget">
+                <div class="col-12 my-5">
+                    <div class="row g-3">
+                        <div class="col-12 col-lg-4" data-aos="zoom-in-up" data-aos-duration="1000" >
+                            <div class="card bg-transparent" style="border:0px;box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;">
+                                <div class="cad-body p-3">
+                                    <div class="row g-0">
+                                        <div class="col-12 col-lg-9">
+                                            <p class="font-widget-content1">Transaction</p>
+                                            <p class="font-widget-content2">2500 +</p>
+                                        </div>
+                                        <div class="col-12 col-lg-3 d-flex align-items-center">
+                                            <div class="text-center">
+                                                <img class="w-75" src="{{ asset ('assets/images/aboutus/icon-transaction.png') }}" alt="Icon Widget">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-12 col-lg-4">
-                        <div class="card bg-transparent shadow">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-12 col-lg-8">
-                                        <p class="font-widget-content1">Application user</p>
-                                        <p class="font-widget-content2">2500 +</p>
-                                    </div>
-                                    <div class="col-12 col-lg-4">
-                                        <div class="text-center">
-                                            <img class="w-50" src="{{ asset ('assets/images/aboutus/icon-user.png') }}" alt="Icon Widget">
+                        <div class="col-12 col-lg-4" data-aos="zoom-in-up" data-aos-duration="1000">
+                            <div class="card bg-transparent" style="border:0px;box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;">
+                                <div class="cad-body p-3">
+                                    <div class="row g-0">
+                                        <div class="col-12 col-lg-9">
+                                            <p class="font-widget-content1">Application user</p>
+                                            <p class="font-widget-content2">2500 +</p>
+                                        </div>
+                                        <div class="col-12 col-lg-3 d-flex align-items-center">
+                                            <div class="text-center">
+                                                <img class="w-50" src="{{ asset ('assets/images/aboutus/icon-user.png') }}" alt="Icon Widget">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-12 col-lg-4">
-                        <div class="card bg-transparent shadow">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-12 col-lg-8">
-                                        <p class="font-widget-content1">Clients</p>
-                                        <p class="font-widget-content2">2500 +</p>
-                                    </div>
-                                    <div class="col-12 col-lg-4">
-                                        <div class="text-center">
-                                            <img class="w-50" src="{{ asset ('assets/images/aboutus/icon-person.png') }}" alt="Icon Widget">
+                        <div class="col-12 col-lg-4" data-aos="zoom-in-up" data-aos-duration="1000">
+                            <div class="card bg-transparent" style="border:0px;box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;">
+                                <div class="cad-body p-3">
+                                    <div class="row g-0">
+                                        <div class="col-12 col-lg-9">
+                                            <p class="font-widget-content1">Clients</p>
+                                            <p class="font-widget-content2">2500 +</p>
+                                        </div>
+                                        <div class="col-12 col-lg-3 d-flex align-items-center">
+                                            <div class="text-center">
+                                                <img class="w-50" src="{{ asset ('assets/images/aboutus/icon-person.png') }}" alt="Icon Widget">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -104,34 +98,36 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="section-aboutus-industry">
-            <div class="container" data-aos="zoom-out-up" data-aos-duration="1000">
-                <div class="row d-flex justify-content-center px-5">
-                    <div class="font-aboutus-industry mx-5">
-                        Industry
-                        <hr class="style-1">
-                    </div>
-                    <div class="icon-card-idustry col-3 mt-5 text-center zoom">
-                        <img class="" src="{{ asset ('assets/images/aboutus/Education.png') }}" alt="Icon Industry">
-                        <p class="font-industry-content">Education</p>
-                    </div>
-                    <div class="icon-card-idustry col-3 mt-5 text-center zoom">
-                        <img class="" src="{{ asset ('assets/images/aboutus/Financial.png') }}" alt="Icon Industry">
-                        <p class="font-industry-content">Financial services</p>
-                    </div>
-                    <div class="icon-card-idustry col-3 mt-5 text-center zoom">
-                        <img class="" src="{{ asset ('assets/images/aboutus/Medical.png') }}" alt="Icon Industry">
-                        <p class="font-industry-content">Medical</p>
-                    </div>
-                    <div class="icon-card-idustry col-3 mt-5 text-center zoom">
-                        <img class="" src="{{ asset ('assets/images/aboutus/oil.png') }}" alt="Icon Industry">
-                        <p class="font-industry-content">Oil and Gas</p>
+                <div class="col-12 my-5">
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <p class="fw-bold" style="font-size:30px;">Several industries have adopted <b class="text-warning">X</b><b style="color:rgb(18, 0, 177);">10S</b> services</p>
+                        </div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-12 col-lg-3 text-center zoom">
+                                    <img class="w-50" src="{{ asset ('assets/images/aboutus/Education.png') }}" alt="Icon Industry">
+                                    <p class="font-industry-content">Education</p>
+                                </div>
+                                <div class="col-12 col-lg-3 text-center zoom">
+                                    <img class="w-50" src="{{ asset ('assets/images/aboutus/Financial.png') }}" alt="Icon Industry">
+                                    <p class="font-industry-content">Financial services</p>
+                                </div>
+                                <div class="col-12 col-lg-3 text-center zoom">
+                                    <img class="w-50" src="{{ asset ('assets/images/aboutus/Medical.png') }}" alt="Icon Industry">
+                                    <p class="font-industry-content">Medical</p>
+                                </div>
+                                <div class="col-12 col-lg-3 text-center zoom">
+                                    <img class="w-50" src="{{ asset ('assets/images/aboutus/oil.png') }}" alt="Icon Industry">
+                                    <p class="font-industry-content">Oil and Gas</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</section>
 
 @endsection
